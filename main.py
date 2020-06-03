5b48eca1f2968674b5026df886d90a087635f7b1
WichoChuletas <fernando.arbovera@gmail.com>
2020-06-02 22:35:34 -0500

    Normalize

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World Flask'
+    user_ip = request.remote_addr
+    return 'Hello World Flask, your IP is {}'.format(user_ip)
 
 
 
